bfd0a57be0fdb7dde3c6da1dce96241f7ac1a669
Peter Botsford <petertoddbotsford@gmail.com>
2021-10-20 14:10:20 -0600

    python practice submission

---
+++ 01_product/product.py
@@ -0,0 +1,11 @@
+def product(a, b):
+    """Return product of a and b.
+
+        >>> product(2, 2)
+        4
+
+        >>> product(2, -2)
+        -4
+    """
+    # for nums in product return a * b
+    return a * b
